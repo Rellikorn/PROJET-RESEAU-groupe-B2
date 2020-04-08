97e2162d7b5d49e11bffacaee4d927ec87e1f3f9
Vilquid <60876471+Vilquid@users.noreply.github.com>
2020-04-08 10:35:17 +0200

    Update Projet1 - Réseaux.c

--- Projet1 - Réseaux.c
+++ Projet1 - Réseaux.c
@@ -2,86 +2,121 @@
 #include <stdlib.h> /* pour exit */
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h> /* pour memset */
+#include <netinet/in.h> /* pour struct sockaddr_in */
+#include <arpa/inet.h> /* pour htons et inet_aton */
+#include <unistd.h> /* pour sleep */
 
-struct sockaddr
-{
-	unsigned short int sa_family; //au choix unsigned 
-	char sa_data[14]; //en fonction de la famille
-};
+#define PORT IPPORT_USERRESERVED // = 5000
+#define LG_MESSAGE 256
 
-struct in_addr
-{
-	unsigned int s_addr;
-}; // une adresse Ipv4 (32 bits)
 
-struct sockaddr_in
-{
-	unsigned short int sin_family; // <- PF_INET
-	unsigned short int sin_port; // <- numéro de port
-	struct in_addr sin_addr; // <- adresse IPv4
-	unsigned char sin_zero[8]; // ajustement pour être compatible avec sockaddr
-};
 
 int main(int argc, char const *argv[])
 {
-	int descripteurSocket;
-	//--- Début de l’étape n°1 : 
-	// Crée un socket de communication
-	descripteurSocket = socket(PF_INET, SOCK_STREAM, 0);
-	/* 0 indique que l’on utilisera le protocole par défaut associé à SOCK_STREAM soit TCP */
-	// Teste la valeur renvoyée par l’appel système socket()
-	
-	if(descripteurSocket < 0) /* échec ? */
-	{
-		perror("socket"); // Affiche le message d’erreu
-		exit(-1); // On sort en indiquant un code erreur
-	}
-	
-	//--Fin de l’étape n°1 !
-	printf("Socket créée avec succès ! (%d)\n", descripteurSocket);
+	int socketEcoute;
+	struct sockaddr_in pointDeRencontreLocal;
 
-	// On ferme la ressource avant de quitter 
-	close(descripteurSocket);
-
-	int descripteurSocket;
-	struct sockaddr_in pointDeRencontreDistant;
 	socklen_t longueurAdresse;
-	
-	// Crée un socket de communication
-	descripteurSocket = socket(PF_INET, SOCK_STREAM, 0);
+	int socketDialogue;
+	struct sockaddr_in pointDeRencontreDistant;
+	char messageEnvoi[LG_MESSAGE]; /* le message de la couche Application ! */
+	char messageRecu[LG_MESSAGE]; /* le message de la couche Application ! */
+	int ecrits, lus; /* nb d’octets ecrits et lus */
+	int retour;
 
-	// Teste la valeur renvoyée par l’appel système socket() 
-	if(descripteurSocket < 0)
+	// Crée un socket de communication
+	socketEcoute = socket(PF_INET, SOCK_STREAM, 0);
+	/* 0 indique que l’on utilisera le protocole par défaut associé à SOCK_STREAM soit TCP */
+	
+	// Teste la valeur renvoyée par l’appel système
+	socket()
+
+	if(socketEcoute < 0) /* échec ? */
 	{
-		perror("socket"); // Affiche le message d’erreur 
+		perror("socket"); // Affiche le message d’erreur
 		exit(-1); // On sort en indiquant un code erreur
 	}
-	
-	printf("Socket créée avec succès ! (%d)\n", descripteurSocket);
-	//--- Début de l’étape n°2 : 
-	// Obtient la longueur en octets de la structure 
-	sockaddr_in longueurAdresse = sizeof(pointDeRencontreDistant);
-	// Initialise à 0 la structure
-	sockaddr_in memset(&pointDeRencontreDistant, 0x00, longueurAdresse);
-	// Renseigne la structure sockaddr_in avec les informations du serveur distant
-	pointDeRencontreDistant.sin_family = PF_INET;
-	// On choisit le numéro de port d’écoute du serveur
-	pointDeRencontreDistant.sin_port = htons(IPPORT_USERRESERVED); // = 5000 
-	// On choisit l’adresse IPv4 du serveur
-	inet_aton("127.0.0.1", &pointDeRencontreDistant.sin_addr);
-	// à modifier selon ses besoins
-	// Débute la connexion vers le processus serveur distant
-	
-	if((connect(descripteurSocket, (struct sockaddr *)&pointDeRencontreDistant, longueurAdresse)) == -1)
+
+	printf("Socket créée avec succès ! (%d)\n", socketEcoute);
+
+	// On prépare l’adresse d’attachement locale
+	longueurAdresse = sizeof(struct sockaddr_in);
+	memset(&pointDeRencontreLocal, 0x00, longueurAdresse);
+	pointDeRencontreLocal.sin_family = PF_INET;
+	pointDeRencontreLocal.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// toutes les interfaces locales disponibles
+	pointDeRencontreLocal.sin_port = htons(PORT);
+
+	// On demande l’attachement local de la socket
+	if((bind(socketEcoute, (struct sockaddr *)&pointDeRencontreLocal, longueurAdresse)) < 0)
 	{
-		perror("connect"); // Affiche le message d’erreur
-		close(descripteurSocket); // On ferme la ressource avant de quitter 
-		exit(-2); // On sort en indiquant un code erreur
+		perror("bind");
+		exit(-2);
 	}
 
-	//--- Fin de l’étape n°2 ! 
-	printf("Connexion au serveur réussie avec succès !\n");
-	// On ferme la ressource avant de quitter close(descripteurSocket); 
+	printf("Socket attachée avec succès !\n");
+	// On fixe la taille de la file d’attente à 5 (pour les demandes de connexion non encoretraitées) if(listen(socketEcoute, 5) < 0) { perror("listen"); exit(-3); }
+	
+	printf("Socket placée en écoute passive ...\n");
+	// boucle d’attente de connexion : en théorie, un serveur attend indéfiniment !
+	
+	while(1)
+	{
+		memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+		memset(messageRecu, 0x00, LG_MESSAGE*sizeof(char));
+		printf("Attente d’une demande de connexion (quitter avec Ctrl-C)\n\n");
+		// c’est un appel bloquant
+		socketDialogue = accept(socketEcoute, (struct sockaddr *)&pointDeRencontreDistant, & longueurAdresse);
+		if (socketDialogue < 0)
+		{
+			perror("accept");
+			close(socketDialogue);
+			close(socketEcoute);
+			exit(-4);
+		}
+		// On réceptionne les données du client (cf. protocole)
+		lus = read(socketDialogue, messageRecu, LG_MESSAGE*sizeof(char));
+		// ici appel bloquant
 
+		switch(lus)
+		{
+			case -1 : /* une erreur ! */
+				perror("read");
+				close(socketDialogue);
+				exit(-5);
+			case 0 : /* la socket est fermée */
+				fprintf(stderr, "La socket a été fermée par le client !\n\n");
+				close(socketDialogue);
+				return 0;
+			default: /* réception de n octets */
+				printf("Message reçu : %s (%d octets)\n\n", messageRecu, lus);
+		}
+
+		// On envoie des données vers le client (cf. protocole)
+		sprintf(messageEnvoi, "ok\n");
+		ecrits = write(socketDialogue, messageEnvoi, strlen(messageEnvoi));
+		
+		switch(ecrits)
+		{
+			case -1 : /* une erreur ! */
+				perror("write");
+				close(socketDialogue)
+				exit(-6);
+			case 0 : /* la socket est fermée */
+				fprintf(stderr, "La socket a été fermée par le client !\n\n");
+				close(socketDialogue);
+			return 0;
+			default: /* envoi de n octets */
+				printf("Message %s envoyé (%d octets)\n\n", messageEnvoi, ecrits);
+		}
+		
+		// On ferme la socket de dialogue et on se replace en attente ...
+		close(socketDialogue);
+	}
+
+	// On ferme la ressource avant de quitter
+	close(socketEcoute);
 	return 0;
 }
